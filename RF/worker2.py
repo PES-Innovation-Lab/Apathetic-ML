eee34ecf6d4862d90b79c93aa73f830c8b51ac56
raunaks42 <42582079+raunaks42@users.noreply.github.com>
2019-06-25 07:05:26 +0000

    Random Forest

---
+++ RF/worker2.py
@@ -0,0 +1,37 @@
+
+import flask
+import subprocess
+
+app = flask.Flask(__name__)
+
+path_to_run = './'          #directory here
+py_name = 'RF(Worker2).py'   #fileName here
+args = ["python3", "{}{}".format(path_to_run, py_name)]
+
+lrw=None
+
+
+@app.route('/api/worker/start', methods = ['GET'])
+def start():
+    global lrw
+    global output
+    if lrw is not None:    #if process is running or has run before
+        return flask.Response(status=409)   #code:conflict
+    else:                   #process never run    
+        lrw=subprocess.Popen(args)
+        return flask.Response(status=202)   #code:accepted
+
+@app.route('/api/worker/stop', methods = ['GET'])
+def stop():
+    global lrw
+    if lrw is not None:    #if process is running or has completed
+        lrw.terminate()
+        lrw=None
+        return flask.Response(status=200)   #code:ok
+    else:                   #process never run
+        return flask.Response(status=403)   #code:forbidden
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=6000)
+
