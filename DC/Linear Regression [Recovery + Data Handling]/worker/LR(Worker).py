5905d43b20d834ae52ed590c57d1f7ef17f1ffe6
raunaks42 <42582079+raunaks42@users.noreply.github.com>
2019-07-16 09:19:19 +0000

    Kafka implementation

--- DC/Linear Regression [Recovery + Data Handling]/worker/LR(Worker).py
+++ DC/Linear Regression [Recovery + Data Handling]/worker/LR(Worker).py
@@ -4,13 +4,13 @@ import numpy as np
 import pandas as pd
 from flask_cors import CORS
 
-app = flask.Flask(__name__)
-CORS(app)
+#CS
+#app = flask.Flask(__name__)
 
-user=None
+#user=None
 
-sesh=requests.Session()
-
+#sesh=requests.Session()
+#CE
 
 class User:
     def __init__(self,learning_rate=0.1):
@@ -27,7 +27,8 @@ class User:
         mse_loss_grad = (y_pred-self.train_y[self.batch_size*step : self.batch_size*step + self.batch_size].reshape(self.batch_size,1))/self.batch_size
         return (self.train_dataset[self.batch_size*step : self.batch_size*step + self.batch_size].T @ mse_loss_grad)*self.learning_rate,np.mean((y_pred-self.train_y[self.batch_size*step : self.batch_size*step + self.batch_size].reshape(self.batch_size,1)))*self.learning_rate       #send back updates
 
-
+#CS
+'''
 @app.route('/api/worker/lr/userinit', methods = ['POST'])
 def userinit():
     global user
@@ -75,9 +76,44 @@ def fitmodel():
     url = 'http://master:5000/api/master/lr/updatemodel'
     sesh.post(url,json={'weights':weights.tolist(),'biases':biases})
     return flask.Response(status = 200)
-    
+'''
+#CE
     
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-    
+    #CS
+    #app.run(host='127.0.0.1', port=5000)
+    user=None
+    consumer = KafkaConsumer('m2w',bootstrap_servers=['localhost:9092'],auto_offset_reset='earliest',value_deserializer=lambda x: loads(x.decode('utf-8')))
+    for msg in consumer:
+        x=ast.literal_eval(msg.value)
+        if x['fun']=='userinit':
+            user=User(x['learning_rate'])
+        elif x['fun']=='initmodel':
+            try:
+                with open("out",'a') as standardout:
+                    print("Initialising model",file=standardout)
+                #train_dataset=np.array(flask.request.json['train_dataset'])
+                #train_y=np.array(flask.request.json['train_y'])
+                dataset = pd.read_csv('USA_Housing.csv')
+                X = dataset.iloc[:,0:5].values
+                y = dataset.iloc[:,5].values
+                with open("out",'a') as standardout:
+                    print("Xshape",X.shape,file=standardout)
 
+                # Feature Scaling
+                from sklearn.preprocessing import StandardScaler
+                sc_X = StandardScaler()
+                X = sc_X.fit_transform(X)
+                with open("out",'a') as standardout:
+                    print("Xshape",X.shape,file=standardout)    
+                batch_size=x['batch_size']
+                user.init_model(X,y,batch_size)
+            except Exception as e:
+                with open("out",'a') as standardout:
+                    print(str(e),file=standardout)
+        elif x['fun']=='fitmodel':
+            (wgrad,bgrad)=user.fit_model(numpy.array(x['weights']),x['biases'],x['step'])
+            producer = KafkaProducer(value_serializer=lambda v: dumps(v).encode('utf-8'),bootstrap_servers = ['localhost:9092'])
+            producer.send('w2m',{'wgrad':wgrad.tolist(),'bgrad':bgrad})
+    #CE
+    
