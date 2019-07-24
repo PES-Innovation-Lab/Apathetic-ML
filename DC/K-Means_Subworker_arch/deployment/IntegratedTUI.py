5334fbe84f7437a718a234703031ca7b7ffd7ae8
metonymic-smokey <48997495+metonymic-smokey@users.noreply.github.com>
2019-07-24 13:53:03 +0530

    Update IntegratedTUI.py

--- DC/K-Means_Subworker_arch/deployment/IntegratedTUI.py
+++ DC/K-Means_Subworker_arch/deployment/IntegratedTUI.py
@@ -7,9 +7,49 @@ import npyscreen
 import os
 x = BT.screen()
 num_workers = x[0]
+model = (x[2])
+
+sub = """apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: subworker0
+  labels:
+    app: server
+    target: subworker0
+spec:
+  selector:
+    matchLabels:
+      target: subworker0
+  replicas: 1
+  template:
+    metadata:
+      labels:
+        target: subworker0
+    spec:
+      containers:
+        - name: hydra
+          image: 192.168.0.10:8080/km_v3_subworker
+          imagePullPolicy: Always
+          env:
+            - name: GET_HOSTS_FROM
+              value: dns
+          command: ["sh","start.sh"]
+          ports:
+            - containerPort: 4000
+            - containerPort: 5000
+            - containerPort: 22
+            - containerPort: 873
+      dnsPolicy: ClusterFirst
+      restartPolicy: Always
+---
+"""
+
+sub = sub.split('\n')
+sub = [x+'\n' for x in sub]
+
 #prints all data from TUI
-for i in x:
-    print(i)
+#for i in x:
+#    print(i)
 
 def worker_add(file):
     f = open(file,'r')
@@ -27,6 +67,14 @@ def worker_add(file):
     for i in range(1,num_workers):
         a = [x.replace('worker0','worker'+str(i)) for x in copy]
         l.extend(a)
+        
+    a = []
+    
+    if(model == 'K-Means Clustering'):    
+        for i in range(0,num_workers):
+             for  j in range(0,2):
+                a = [x.replace('subworker0','subworker'+str(i)+str(j)) for x in sub]
+                l.extend(a)
 
     l.extend(lines[pos_m:])
     f.close()
